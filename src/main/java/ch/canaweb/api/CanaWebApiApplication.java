2589370253df451ed587b443a8b150e276317b0c
lukas <lukas.luetolf@gmail.com>
2022-06-22 14:21:23 +0200

    Clean up and define configuration for local and test.

--- src/main/java/ch/canaweb/api/CanaWebApiApplication.java
+++ src/main/java/ch/canaweb/api/CanaWebApiApplication.java
@@ -1,19 +1,31 @@
 package ch.canaweb.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CanaWebApiApplication {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(CanaWebApiApplication.class, args);
+    }
 
-        String firestoreCredentialsLocation = ctx.getEnvironment().getProperty("spring.cloud.gcp.firestore.project-id");
-        System.out.println("GCP Config:" + firestoreCredentialsLocation);
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        this.logger.info(env.getProperty("app.name") + " running...");
+        this.logger.info(env.getProperty("app.description"));
     }
 
 }
